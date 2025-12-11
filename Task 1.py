091683be3e4789ffc0e438543150b52f05ac1c87
kkapur97 <48401335+kkapur97@users.noreply.github.com>
2025-12-11 18:45:23 +0530

    Initial commit with python program
    Uploaded both the python code

---
+++ Task 1.py
@@ -0,0 +1,22 @@
+
+### Performing Mathematical operations on the given numberss
+
+a = int(input(" Enter the first number : "))
+
+b = int(input(" Enter the second number : "))
+
+add = a + b
+
+sub = a - b
+
+mult = a * b
+
+div = a / b
+
+print(" Addition of given numbers is ", add)
+
+print(" Subtractiom of given numbers is " , sub)
+
+print(" Multiplication of given numbers is ", mult)
+
+print(" division of given numbers is ", div)
---
+++ Task 2.py
@@ -0,0 +1,8 @@
+
+first_name = input(" enter the first name : ")
+
+last_name = input(" enter the last name : ")
+
+full_name = first_name + " " + last_name
+
+print( f" Hello, {full_name}! Welcome to the python program " )
